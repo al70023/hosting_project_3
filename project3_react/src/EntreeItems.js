034abe60872a7e9093d0f248861500d54a9ff624
al70023 <apantazopol@gmail.com>
2022-11-11 00:20:16 -0600

    add entree items to current order

--- project3_react/src/EntreeItems.js
+++ project3_react/src/EntreeItems.js
@@ -1,69 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { useState, useEffect, Fragment } from 'react';
 import './EntreeItems.css';
+
 
 function EntreeItems() {
     // Data state variable defaulted to an empty array (for printing out the data)
     const [entreeItemsData, setEntreeItemsData] = useState([]);
 
-    // Data state variable for the data from the add entree item form
-    const [addFormData, setAddFormData] = useState({
-        item_name: '',
-        item_price: '',
-    })
+    const [items, setItemsOrdered] = useState([]);
 
-    const [editEntreeItemID, setEditEntreeItemID] = useState(null);
+    // const handleAddToOrder = (entreeItem) => {
+
+    //     const orderedItems = [...items, entreeItem];
+    //     setItemsOrdered(orderedItems);
+    // }
 
     const fetchEntreeItems = () => {
         fetch('http://localhost:3001/EntreeItems')
-        .then(res => res.json())
-        .then(json => setEntreeItemsData(json))
+            .then(res => res.json())
+            .then(json => setEntreeItemsData(json))
     }
-    
+
     // Call the function on the component mount
     useEffect(() => {
         fetchEntreeItems();
     }, []);
 
-    console.log(entreeItemsData);
-    return(
-        <div className = "entreeItems">
+    return (
+        <div className="entreeItems">
             <h1> Entree Items </h1>
 
 
-            <div className = "entreeItemTable">
+            <div className="entreeItemTable">
                 <form>
-                <table>
-                    <thead>
-                        <tr>
-                            {/* Table Column Headers */}
-                            <th>Name</th> 
-                            <th>Price</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {entreeItemsData.map((entreeItem) => (
+                    <table>
+                        <thead>
                             <tr>
-                                <td>{entreeItem.item_name}</td>
-                                <td>{entreeItem.item_price}</td>
-                                <td><button>Add Item</button></td>
-
+                                {/* Table Column Headers */}
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th></th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {entreeItemsData.map((entreeItem) => (
+                                <tr>
+                                    <td name="name">{entreeItem.item_name}</td>
+                                    <td name="price">{entreeItem.item_price}</td>
+                                    <td><button type="button" onClick={() => setItemsOrdered([...items, entreeItem])}>Add Item</button></td>
+
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </form>
-                        
-                
+
+
 
             </div>
-        
 
-            
+            <h1>Orders</h1>
+            <ol>
+                {items.map((item) =>
+                    <li>
+                        {item.item_name}    {item.item_price}
+                    </li>
+                )}
+            </ol>
+
         </div>
     );
-
 }
 
 export default EntreeItems;
