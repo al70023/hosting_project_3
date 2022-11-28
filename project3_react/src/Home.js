2cf6a12ffb5524f373157217d863c08ada70de91
vivian-zheng <vivian.x.zheng@gmail.com>
2022-11-28 15:28:50 -0600

    working on gitignore

--- project3_react/src/Home.js
+++ project3_react/src/Home.js
@@ -10,15 +10,7 @@ function Home(){
         <img class="mx-auto mt-24"src="https://upload.wikimedia.org/wikipedia/commons/0/02/Chick-fil-A_Logo.svg"/>
         </div>
 
-        <div id="google_translate_element"></div>
-
-        <script type="text/javascript">
-        function googleTranslateElementInit() {
-            new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element') }
-        </script>
-
-        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-
+        
         
     </div>
     );
