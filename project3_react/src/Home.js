b520e4a7b8fd6c9064438081683a92480ad95a97
al70023 <apantazopol@gmail.com>
2022-12-05 12:08:17 -0600

    google translate done

--- project3_react/src/Home.js
+++ project3_react/src/Home.js
@@ -48,8 +48,6 @@ function Home(){
     return(
         <div>
             <div class="homeTopContainer">
-        
-            <p class="text-lg mt-9">Welcome to Chick-fil-A!</p>
             <img class="mx-auto mt-24"src="https://upload.wikimedia.org/wikipedia/commons/0/02/Chick-fil-A_Logo.svg"/>
         
             <form action="/ServerHome" method="POST">
